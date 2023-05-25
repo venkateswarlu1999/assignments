141b94b7a1b75888d661e003cb9a42fc87b74313
unknown <venkateswarlu7.marolix@gmail.com>
2023-05-25 12:10:50 +0530

    string methods

--- 23-05-23/stringmethods.py
+++ 23-05-23/stringmethods.py
@@ -51,9 +51,21 @@ while index<len(s):
         index = j+len(sub)
 print('given string: ',s)
 print('given sub string: ',sub)
-print(positions)
+print('the given sub string found at: 'positions)
 
 '''
+'''
+s = input('Enter any string: ')
+sub = input('Enter any sub string: ')
+index=s.find(sub,0,len(s))
+
+if index >=0:
+    print('your sub string found at index of: ',index)
+else:
+    print('your sub string is not found')
+
+'''
+
 # 3. input = ABBCDEFFGHILJK
 # output= ABCDEFGHIJK
 # using join function
@@ -68,4 +80,17 @@ ca = ''.join(sorted(set(a)))
 print('finall')
 print(ca)
 
+'''
+'''
+l=[10,20,[30,40],50]
+print(l[0:2]+[l[2][1]])
+print(l[0:2]+l[2][:1])
+
+print(l[0:2]+l[2][:2])
+print(l[0:2]+l[2][::2])
+print(l[0:2]+l[2][1:2])
+print(l[0:2]+l[2][0:2])
+print(l[0:2]+l[2][0:1])
+print(l[2][0])
+print(l[2][1])
 '''
